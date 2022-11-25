3ee67434ad3643ea56e816a7781906153f8a19bb
Thilina Ratnayaka <thilinarmtb@gmail.com>
2022-11-25 15:16:47 -0600

    Fetch immediate neighbors

--- src/reorder.c
+++ src/reorder.c
@@ -2,6 +2,8 @@
 
 extern unsigned get_proc_bin(uint id, uint np);
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 static void sfree(void *p, const char *file, unsigned line) {
   if (p)
     free(p);
@@ -232,4 +234,160 @@ void fparrsb_reorder_dofs(long long *nid, int *n, int *nv, long long *ids,
   *err = 0;
 }
 
+void parrsb_fetch_nbrs(unsigned *nei, long long *eids, unsigned nv,
+                       long long *vids, double *xyz, int *mask,
+                       const MPI_Comm comm, unsigned maxne) {
+  size_t ne = *nei, size = nv;
+  size *= ne;
+
+  struct vtx_t {
+    ulong id;
+    uint p, o, seq;
+  };
+
+  struct array vtxs;
+  array_init(struct vtx_t, &vtxs, size);
+
+  struct comm ci;
+  comm_init(&ci, comm);
+
+  struct vtx_t vt;
+  for (uint e = 0; e < ne; e++) {
+    for (unsigned v = 0; v < nv; v++) {
+      vt.id = vids[e * nv + v], vt.o = ci.id, vt.p = vt.id % ci.np, vt.seq = e;
+      array_cat(struct vtx_t, &vtxs, &vt, 1);
+    }
+  }
+
+  struct crystal cr;
+  crystal_init(&cr, &ci);
+
+  sarray_transfer(struct vtx_t, &vtxs, p, 1, &cr);
+
+  buffer bfr;
+  buffer_init(&bfr, 1024);
+  sarray_sort(struct vtx_t, vtxs.ptr, vtxs.n, id, 1, &bfr);
+
+  struct array vtx2p;
+  array_init(struct vtx_t, &vtx2p, vtxs.n);
+
+  struct vtx_t *pv = (struct vtx_t *)vtxs.ptr;
+  uint s = 0;
+  while (s < vtxs.n) {
+    uint e = s + 1;
+    while (e < vtxs.n && pv[s].id == pv[e].id)
+      e++;
+    for (unsigned i = s; i < e; i++) {
+      vt = pv[i];
+      for (unsigned j = s; j < e; j++) {
+        vt.o = pv[j].o;
+        array_cat(struct vtx_t, &vtx2p, &vt, 1);
+      }
+    }
+    s = e;
+  }
+  array_free(&vtxs);
+
+  sarray_transfer(struct vtx_t, &vtx2p, p, 0, &cr);
+  sarray_sort_2(struct vtx_t, vtx2p.ptr, vtx2p.n, seq, 0, o, 0, &bfr);
+
+  uint *offs = tcalloc(uint, ne + 1);
+  uint *proc = tcalloc(uint, ne * 4), nproc = ne * 4;
+  pv = (struct vtx_t *)vtx2p.ptr, s = 0;
+  while (s < vtx2p.n) {
+    uint seq = pv[s].seq, e = s + 1;
+    while (e < vtx2p.n && seq == pv[e].seq)
+      e++;
+
+    uint work[1024], n = 1;
+    work[0] = pv[s].o;
+    for (uint i = s + 1; i < e; i++) {
+      if (work[n - 1] != pv[i].o)
+        work[n] = pv[i].o, n++;
+    }
+
+    offs[seq + 1] = offs[seq] + n;
+    if (offs[seq + 1] >= nproc) {
+      nproc = offs[seq + 1] + nproc / 2 + 1;
+      proc = trealloc(uint, proc, nproc);
+    }
+    for (uint i = 0, j = offs[seq]; i < n; i++)
+      proc[j + i] = work[i];
+    s = e;
+  }
+  array_free(&vtx2p);
+
+  struct elem_t {
+    ulong eid, vid[8];
+    double xyz[3 * 8];
+    uint p;
+  };
+
+  struct array elems;
+  array_init(struct elem_t, &elems, size);
+
+  unsigned nd = (nv == 8) ? 3 : 2;
+  struct elem_t et;
+  for (uint e = 0; e < ne; e++) {
+    et.eid = eids[e];
+    for (unsigned v = 0; v < nv; v++) {
+      et.vid[v] = vids[e * nv + v];
+      for (unsigned d = 0; d < nd; d++)
+        et.xyz[v * nd + d] = xyz[e * nv * nd + v * nd + d];
+    }
+    for (uint s = offs[e]; s < offs[e + 1]; s++) {
+      et.p = proc[s];
+      array_cat(struct elem_t, &elems, &et, 1);
+    }
+  }
+  tfree(offs), tfree(proc);
+
+  sarray_transfer(struct elem_t, &elems, p, 1, &cr);
+  crystal_free(&cr);
+
+  sint err = (elems.n > maxne), wrk;
+  comm_allreduce(&ci, gs_int, gs_add, &err, 1, &wrk);
+  if (err > 0) {
+    if (ci.id == 0) {
+      fprintf(stderr, "maxne = %u is not large enough !\n", maxne);
+      fflush(stderr);
+    }
+    buffer_free(&bfr), array_free(&elems), comm_free(&ci);
+    exit(1);
+  }
+
+  sarray_sort(struct elem_t, elems.ptr, elems.n, eid, 1, &bfr);
+  buffer_free(&bfr);
+
+  *nei = elems.n;
+  if (elems.n > 0) {
+    struct elem_t *pe = (struct elem_t *)elems.ptr;
+    for (uint e = 0; e < elems.n; e++) {
+      eids[e] = pe[e].eid;
+      mask[e] = (pe[e].p != ci.id);
+      for (unsigned v = 0; v < nv; v++) {
+        vids[e * nv + v] = pe[e].vid[v];
+        for (unsigned d = 0; d < nv; d++)
+          xyz[e * nv + v * nd + d] = pe[e].xyz[v * nd + d];
+      }
+    }
+  }
+
+  array_free(&elems), comm_free(&ci);
+}
+
+#define fparrsb_fetch_nbrs                                                     \
+  FORTRAN_UNPREFIXED(fparrsb_fetch_nbrs, FPARRSB_FETCH_NBRS)
+void fparrsb_fetch_nbrs(int *nei, long long *eids, int *nv, long long *vids,
+                        double *xyz, int *mask, MPI_Fint *comm, int *maxne,
+                        int *err) {
+  *err = 1;
+  MPI_Comm c = MPI_Comm_f2c(*comm);
+  unsigned ne = *nei;
+  parrsb_fetch_nbrs(&ne, eids, *nv, vids, xyz, mask, c, *maxne);
+  *nei = ne;
+  *err = 0;
+}
+
 #undef tfree
+#undef MIN
