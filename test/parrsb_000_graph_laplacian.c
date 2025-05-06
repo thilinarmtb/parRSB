3c21b77e0855f3a600bdb74d36cf33f9e1ceaf86
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-05-06 15:30:06 -0500

    Refactor and add new laplacian tests

--- test/parrsb_000_graph_laplacian.c
+++ test/parrsb_000_graph_laplacian.c
@@ -12,11 +12,10 @@ static int test_brick_00(const element_info ei, struct crystal *cr,
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
-
   uint size = laplacian_size(l);
   laplacian_op(NULL, l, NULL);
-
   laplacian_free(&l);
+
   graph_restore(NULL, &nlist, cr, bfr);
   graph_free(&g);
 
@@ -35,12 +34,11 @@ static int test_brick_05(const element_info ei, struct crystal *cr,
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
-
   uint size = laplacian_size(l);
   scalar u = 1, v = 1;
   laplacian_op(&v, l, &u);
-
   laplacian_free(&l);
+
   graph_restore(NULL, &nlist, cr, bfr);
   graph_free(&g);
 
@@ -62,12 +60,11 @@ static int test_brick_10(const element_info ei, struct crystal *cr,
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
-
   uint size = laplacian_size(l);
   scalar u = 1, v = 1;
   laplacian_op(&v, l, &u);
-
   laplacian_free(&l);
+
   graph_restore(NULL, &nlist, cr, bfr);
   graph_free(&g);
 
@@ -86,12 +83,11 @@ static int test_brick_15(const element_info ei, struct crystal *cr,
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
-
   uint size = laplacian_size(l);
   scalar u = 1, v = 1;
   laplacian_op(&v, l, &u);
-
   laplacian_free(&l);
+
   graph_restore(NULL, &nlist, cr, bfr);
   graph_free(&g);
 
@@ -99,6 +95,36 @@ static int test_brick_15(const element_info ei, struct crystal *cr,
 }
 
 static int test_brick_20(const element_info ei, struct crystal *cr,
+                         buffer *bfr) {
+  const struct comm *c = &(cr->comm);
+
+#define SIZE 2
+
+  graph_t g;
+  graph_init_brick(&g, SIZE, c->c);
+
+  struct array nlist;
+  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
+
+  laplacian l;
+  laplacian_init(&l, &nlist, ei, c);
+  scalar u[SIZE];
+  scalar v[SIZE];
+  for (uint i = 0; i < SIZE; i++) u[i] = 1;
+  laplacian_op(v, l, u);
+  laplacian_free(&l);
+
+  scalar normv = norm2(v, SIZE);
+
+#undef SIZE
+
+  graph_restore(NULL, &nlist, cr, bfr);
+  graph_free(&g);
+
+  parrsb_test(normv < PARRSB_TEST_EPS, c);
+}
+
+static int test_brick_25(const element_info ei, struct crystal *cr,
                          buffer *bfr) {
   const struct comm *c = &(cr->comm);
 
@@ -110,17 +136,16 @@ static int test_brick_20(const element_info ei, struct crystal *cr,
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
-
   uint size = laplacian_size(l);
   scalar *u = tcalloc(scalar, size);
   scalar *v = tcalloc(scalar, size);
   for (uint i = 0; i < size; i++) u[i] = 1;
   laplacian_op(v, l, u);
+  laplacian_free(&l);
 
   scalar normv = norm2(v, size);
-
   free(u), free(v);
-  laplacian_free(&l);
+
   graph_restore(NULL, &nlist, cr, bfr);
   graph_free(&g);
 
@@ -149,6 +174,7 @@ int main(int argc, char *argv[]) {
   err |= test_brick_10(ei, &cr, &bfr);
   err |= test_brick_15(ei, &cr, &bfr);
   err |= test_brick_20(ei, &cr, &bfr);
+  err |= test_brick_25(ei, &cr, &bfr);
 
   graph_element_info_free(&ei);
   buffer_free(&bfr);
