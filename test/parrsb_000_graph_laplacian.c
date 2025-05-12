13070f04ee97f776cbc638da53c4a7c8f3b74425
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-05-12 10:01:20 -0500

    Refactor tests

--- test/parrsb_000_graph_laplacian.c
+++ test/parrsb_000_graph_laplacian.c
@@ -1,155 +1,69 @@
 #include "parrsb_test.h"
 
-static int test_brick_00(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
+static int test_base(const element_info ei, unsigned n, graph_type_t type,
+                     struct crystal *cr, buffer *bfr) {
   const struct comm *c = &(cr->comm);
 
   graph_t g;
-  graph_init(&g, 0, PATH, c->c);
+  graph_init(&g, n, type, c->c);
 
   struct array nlist;
   graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
 
   laplacian l;
   laplacian_init(&l, &nlist, ei, c);
+
   uint size = laplacian_size(l);
-  laplacian_op(NULL, l, NULL);
-  laplacian_free(&l);
+  scalar *u = 0, *v = 0;
 
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
+  if (size == 0) {
+    laplacian_op(NULL, l, NULL);
+    goto check_and_finalize;
+  } else {
+    u = tcalloc(scalar, 2 * size), v = u + size;
+    for (unsigned i = 0; i < 2 * size; i++) u[i] = 1;
 
-  parrsb_test(size == 0, c);
-}
+    laplacian_op(v, l, u);
+  }
 
-static int test_brick_05(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
-  const struct comm *c = &(cr->comm);
-
-  graph_t g;
-  graph_init(&g, 1, PATH, c->c);
-
-  struct array nlist;
-  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
-
-  laplacian l;
-  laplacian_init(&l, &nlist, ei, c);
-  uint size = laplacian_size(l);
-  scalar u = 1, v = 1;
-  laplacian_op(&v, l, &u);
-  laplacian_free(&l);
-
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
-
-  parrsb_test(size == 0 || fabs(v) < PARRSB_TEST_EPS, c);
-}
-
-static int test_brick_10(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
-  const struct comm *c = &(cr->comm);
-
-  graph_t g;
-  if (c->id >= c->np / 2)
-    graph_init(&g, 1, PATH, c->c);
-  else
-    graph_init(&g, 0, PATH, c->c);
-
-  struct array nlist;
-  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
-
-  laplacian l;
-  laplacian_init(&l, &nlist, ei, c);
-  uint size = laplacian_size(l);
-  scalar u = 1, v = 1;
-  laplacian_op(&v, l, &u);
-  laplacian_free(&l);
-
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
-
-  parrsb_test(size == 0 || fabs(v) < PARRSB_TEST_EPS, c);
-}
-
-static int test_brick_15(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
-  const struct comm *c = &(cr->comm);
-
-  graph_t g;
-  graph_init(&g, c->id % 2, PATH, c->c);
-
-  struct array nlist;
-  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
-
-  laplacian l;
-  laplacian_init(&l, &nlist, ei, c);
-  uint size = laplacian_size(l);
-  scalar u = 1, v = 1;
-  laplacian_op(&v, l, &u);
-  laplacian_free(&l);
-
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
-
-  parrsb_test(size == 0 || fabs(v) < PARRSB_TEST_EPS, c);
-}
-
-static int test_brick_20(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
-  const struct comm *c = &(cr->comm);
-
-#define SIZE 2
-
-  graph_t g;
-  graph_init(&g, SIZE, PATH, c->c);
-
-  struct array nlist;
-  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
-
-  laplacian l;
-  laplacian_init(&l, &nlist, ei, c);
-  scalar u[SIZE];
-  scalar v[SIZE];
-  for (uint i = 0; i < SIZE; i++) v[i] = u[i] = 1;
-  laplacian_op(v, l, u);
-  laplacian_free(&l);
-
-  scalar normv = norm2(v, SIZE);
-
-#undef SIZE
-
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
-
-  parrsb_test(normv < PARRSB_TEST_EPS, c);
-}
-
-static int test_brick_25(const element_info ei, struct crystal *cr,
-                         buffer *bfr) {
-  const struct comm *c = &(cr->comm);
-
-  graph_t g;
-  graph_init(&g, c->id, PATH, c->c);
-
-  struct array nlist;
-  graph_load_balance(&nlist, g->n, g->nodes, g->offsets, g->neighbors, cr, bfr);
-
-  laplacian l;
-  laplacian_init(&l, &nlist, ei, c);
-  uint size = laplacian_size(l);
-  scalar *u = tcalloc(scalar, size);
-  scalar *v = tcalloc(scalar, size);
-  for (uint i = 0; i < size; i++) u[i] = 1;
-  laplacian_op(v, l, u);
-  laplacian_free(&l);
-
+check_and_finalize:
   scalar normv = norm2(v, size);
-  free(u), free(v);
-
-  graph_restore(NULL, &nlist, cr, bfr);
-  graph_free(&g);
-
+  free(u);
+  laplacian_free(&l);
   parrsb_test(size == 0 || normv < PARRSB_TEST_EPS, c);
+}
+
+static int test_path_00(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  return test_base(ei, 0, PATH, cr, bfr);
+}
+
+static int test_path_05(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  return test_base(ei, 1, PATH, cr, bfr);
+}
+
+static int test_path_10(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  return test_base(ei, 2, PATH, cr, bfr);
+}
+
+static int test_path_15(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  const struct comm *c = &(cr->comm);
+  return test_base(ei, c->id % 2, PATH, cr, bfr);
+}
+
+static int test_path_20(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  const struct comm *c = &(cr->comm);
+  return test_base(ei, c->id >= c->np / 2, PATH, cr, bfr);
+}
+
+static int test_path_25(const element_info ei, struct crystal *cr,
+                        buffer *bfr) {
+  const struct comm *c = &(cr->comm);
+  return test_base(ei, c->id, PATH, cr, bfr);
 }
 
 int main(int argc, char *argv[]) {
@@ -169,12 +83,12 @@ int main(int argc, char *argv[]) {
   graph_element_info_init(&ei);
 
   int err = 0;
-  err |= test_brick_00(ei, &cr, &bfr);
-  err |= test_brick_05(ei, &cr, &bfr);
-  err |= test_brick_10(ei, &cr, &bfr);
-  err |= test_brick_15(ei, &cr, &bfr);
-  err |= test_brick_20(ei, &cr, &bfr);
-  err |= test_brick_25(ei, &cr, &bfr);
+  err |= test_path_00(ei, &cr, &bfr);
+  err |= test_path_05(ei, &cr, &bfr);
+  err |= test_path_10(ei, &cr, &bfr);
+  err |= test_path_15(ei, &cr, &bfr);
+  err |= test_path_20(ei, &cr, &bfr);
+  err |= test_path_25(ei, &cr, &bfr);
 
   graph_element_info_free(&ei);
   buffer_free(&bfr);
