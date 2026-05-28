d87f567092a7fb0bd0276f33895d4b4de1c2c080
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-05-28 20:44:48 +0000

    Get rid of parrsb_options_copy()

--- src/parrsb.h
+++ src/parrsb.h
@@ -18,8 +18,6 @@ int parrsb_options_set_partitioner(parrsb_options_t options, int partitioner);
 
 int parrsb_options_set_rsb_algo(parrsb_options_t options, int algo);
 
-int parrsb_options_copy(parrsb_options_t *dest, const parrsb_options_t src);
-
 void parrsb_options_print(const parrsb_options_t options);
 
 int parrsb_options_free(parrsb_options_t *options);
